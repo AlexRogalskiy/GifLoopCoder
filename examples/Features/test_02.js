ef09b3fd8f9b73e2d6591612c0b8d54908746e80
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2016-07-05 10:11:38 +0300

    Changeset test_02.js

---
+++ examples/Features/test_02.js
@@ -0,0 +1,45 @@
+function onGLC(glc) {
+    glc.loop();
+//     glc.size(400, 400);
+//     glc.setDuration(5);
+//     glc.setFPS(20);
+//     glc.setMode('single');
+//     glc.setEasing(false);
+    var list = glc.renderList,
+        width = glc.w,
+        height = glc.h,
+        color = glc.color;
+
+// 	list.addRect({
+//         x: width / 2,
+//         y: height / 2,
+//         w: [200, 50],
+//         h: [50, 200],
+//         fillStyle: ['red', 'yellow'],
+//         stroke: [false, true],
+//         lineWidth: 20,
+//     });
+    
+//     list.addText({
+//         text: ['hello', 'world'],
+//         fontSize: 50,
+//         x: width / 2,
+//         y: height / 2
+//     });
+    
+//     list.addPoly({
+//         x: width / 2,
+//         y: height / 2,
+//         radius: 150,
+//         sides: 5,
+//         fillStyle: ['red', 'green', 'blue']
+//     })
+    
+//     list.addPath({
+//         path: [
+//         	[0, 0, 100, 50, 50, 100, 100, 100],
+//             [100, 50, 150, 250, 150, 120, 300, 170]
+//         ]
+//     });
+
+}
